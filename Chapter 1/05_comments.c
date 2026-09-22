0b5cfd12d05239f4f3bd17a9760af429a67b144c
ShreyasS15 <shreyass2026@gmail.com>
2026-09-22 22:00:26 +0530

    Add chapter 1 exercises

---
+++ Chapter 1/05_comments.c
@@ -0,0 +1,7 @@
+int main() {
+    // I am Shreyas S.. I am 20 years old
+    int a;
+    a=6*3;
+    printf("The output of the program is %d" , a);
+    return 0;
+}
---
+++ Chapter 1/06.multiline.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    I am Shreyas S.. I am 20 years old
+    I want a job in google
+    */
+
+
+    int a;
+    a=6*3;
+    printf("The output of the program is %d" , a);
+    return 0;
+}
